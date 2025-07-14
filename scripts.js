36f1bc346f5724356cf592d61de8d81050e96467
gustavosrosa <g.dsantosrosa@gmail.com>
2025-07-14 09:42:38 -0300

    TDLT: FEAT: Validando e utilizando local storage

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const NAME_LOCAL_STORAGE = "todos";
+
 const todosApp = {
 
     data() {
@@ -11,15 +13,30 @@ const todosApp = {
     methods: {
         clearTasks() {
             this.tasks = [];
+            this.setItemStorage(this.tasks);
         },
         addNewTask() {
             if (this.newTask.title.trim() !== '') {
                 this.tasks.push(this.newTask);
+
+                this.newTask = {
+                    done: false,
+                };
             }
-            this.newTask = {
-                done: false,
-            };
+            this.setItemStorage(this.tasks);
+        },
+        setItemStorage(task) {
+            window.localStorage.setItem(NAME_LOCAL_STORAGE, JSON.stringify(task));
+        },
+        getItemStorage() {
+            return window.localStorage.getItem(NAME_LOCAL_STORAGE);
         }
+    },
+    created() {
+        this.tasks = this.getItemStorage() ? JSON.parse(this.getItemStorage()) : this.tasks;
+    },
+    updated() {
+        this.setItemStorage(this.tasks);
     }
 
 };
